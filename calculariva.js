d14bd28db88406f1ad656c3d5a49f565c860d9f4
201194523-bit <201194523@a.universidaduniq.com>
2026-08-07 10:18:51 -0600

    Add files via upload
    En esta práctica implementé la sintaxis moderna de funciones flecha (arrow functions) en JavaScript para optimizar la estructura del código mediante operaciones concisas[cite: 2]. Aprendí a definir funciones de una sola línea con retorno implícito para realizar cálculos matemáticos como el IVA, conversión de divisas y potencias cuadradas[cite: 2]. Esto me permitió analizar la importancia de escribir código más limpio, legible y modular dentro del desarrollo web frontend.

---
+++ calculariva.js
@@ -0,0 +1,7 @@
+const calcularIVA = (precio) => precio * 0.16;
+const pesosADolares = (pesos) => pesos / 17;
+const cuadrado = (n) => n * n;
+
+console.log("IVA:", calcularIVA(100));
+console.log("Dólares:", pesosADolares(170));
+console.log("Cuadrado:", cuadrado(5));
